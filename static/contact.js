7f8bc2cf010e28aaa845e6fc5743c7e005bfc483
OT <jakelovz@gmail.com>
2020-11-23 19:57:06 -0800

    added some form validation for contact form

---
+++ static/contact.js
@@ -0,0 +1,47 @@
+console.log('Working!')
+
+const form = document.getElementById('form')
+const email = document.getElementById('email')
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+let checker = true;
+form.addEventListener('submit', (e)=> {
+
+	// validate(email,e)
+	// validate(message,e)
+	if ( validate(email,e) === validate(message,e) === checker ) {
+		console.log('submiting...', checker)
+	}
+
+	// console.log( validate(email,e) === validate(message,e), 'COMPARE', checker )
+	// console.log( 'email:', validate(email,e), 'massage: ',validate(message,e), 'checker:',checker)
+})
+
+function validate(tag, target='') {
+	let value = tag.value
+	if ( value !== '') {
+		checker = true
+		tag.style.border = '1px solid rgb(35, 255, 57)'
+		return true
+
+	} else {
+		tag.style.border = '1px solid rgb(255, 57, 35)';
+		checker = false
+		target.preventDefault()
+		return false
+	}
+	
+}
+
+
+
+
+//   validate form
+// - validate form (validate returns true or false)
+// 	- 1 check inputs for empty value
+		// - check if value is ''
+		// - if its empty se border to red and prevent submit
+		// - 
+// 	- 2 
+// - send info to server
+// 	- gather all required info & send to server
